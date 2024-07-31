177722f856dceb378bfc158e04d6e05b65ca1cac
Anton Rak <anton_rak@epam.com>
2024-07-31 23:55:04 +0200

    addedDeleteCardsFunction

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,4 +1,34 @@
+const {pages} = require('../po/pages/index');
 
+
+async function deleteCards (){
+    await browser.refresh();
+    await browser.pause(2000);
+    await pages('dashboard').headerComponent.rootEl.isDisplayed();
+    await pages('dashboard').tasksTableComponent.rootEl.isDisplayed();
+    const a = await $$('.task-list_row td.task-column span');
+    const length = a.length;
+    console.log(`our length is ${length}`)
+
+    for (let i = 0; i < length; i ++){
+        console.log(`================`)
+        console.log(`I am here! delete the ${i} card`)
+        //click on task card 1
+        await pages('dashboard').taskItemComponent(1).rootEl.click();
+        //create card form is dispalyed
+        await pages('dashboard').taskForm.rootEl.waitForDisplayed(); 
+
+        //delete the card 1
+        await pages('dashboard').taskForm.deleteBtn.click();
+        await pages('dashboard').taskForm.rootEl.waitForDisplayed({reverse: true});
+        await browser.refresh();
+        await browser.pause(2000);
+        await pages('dashboard').headerComponent.rootEl.isDisplayed();
+        await pages('dashboard').tasksTableComponent.rootEl.isDisplayed();
+
+    }
+    await pages('dashboard').taskItemComponent(1).rootEl.waitForDisplayed({reverse: true});
+}
 
 function formatDates(string){
     const arr = string.split(' ');
@@ -36,5 +66,6 @@ async function dueDate(){
 module.exports = {
     formatDates,
     todayCreationDate,
-    dueDate
+    dueDate,
+    deleteCards
 }
