86c2375f1e108189cd389b186d49ca13ffea4e6a
Anton Rak <anton_rak@epam.com>
2024-07-31 19:22:34 +0200

    addingSecondScenario

---
+++ src/utils/functions.js
@@ -0,0 +1,33 @@
+
+
+function formatDates(string){
+    const arr = string.split(' ');
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]; 
+    let dayUpd; 
+    let monthInNumber;
+    let year;
+    for(let i = 0; i < arr.length; i ++){
+        if (i === 0){
+        monthInNumber = months.indexOf(arr[i]) + 1;
+        console.log();
+        } else if (i === 1){
+           dayUpd = arr[i].replace(',', '');
+        } else if (i === 2){
+            year = arr[i];
+        }
+    }
+    return (`${monthInNumber}/${dayUpd}/${year}`);
+}
+
+const todayCreationDate = `${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`
+
+
+
+
+
+
+
+module.exports = {
+    formatDates,
+    todayCreationDate
+}
